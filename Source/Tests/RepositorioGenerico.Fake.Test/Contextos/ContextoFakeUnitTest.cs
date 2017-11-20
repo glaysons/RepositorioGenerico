bcc86427aa28fba0ca29c398fa8feeaea7159286
glaysons <glaysons@gmail.com>
2017-11-20 07:36:48 -0300

    Testes movidos e novos criados para fabrica de contextos.

--- Source/Tests/RepositorioGenerico.Fake.Test/Contextos/ContextoFakeUnitTest.cs
+++ Source/Tests/RepositorioGenerico.Fake.Test/Contextos/ContextoFakeUnitTest.cs
@@ -4,6 +4,8 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RepositorioGenerico.Fake.Contextos;
 using RepositorioGenerico.Test.Objetos;
 using System;
+using RepositorioGenerico.Pattern.Contextos;
+using RepositorioGenerico.Pattern.Buscadores;
 
 namespace RepositorioGenerico.Fake.Test.Contextos
 {
@@ -61,7 +63,72 @@ namespace RepositorioGenerico.Fake.Test.Contextos
 		}
 
 		[TestMethod]
-		public void SeAdicionarUmaListaDeRegistrosNoContextoORepositorioCorretoDeveserModificado()
+		public void SeCarregarRegistrosOContextoDeveConterRegistrosParaBuscaApenas()
+		{
+			var contexto = CriarContextoParaTestes();
+			var repositorio = contexto.Repositorio<ObjetoDeTestes>();
+
+			repositorio.Quantidade
+				.Should().Be(0);
+
+			repositorio.Buscar.Todos()
+				.Should()
+				.HaveCount(5);
+		}
+
+		private IContexto CriarContextoParaTestes()
+		{
+			var contexto = FabricaFake.CriarContexto();
+
+			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 1, Nome = "Teste de cadastros do nome A" });
+			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 2, Nome = "Teste de cadastros do nome B" });
+			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 3, Nome = "Teste de cadastros do nome C" });
+			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 4, Nome = "Teste de cadastros do nome D" });
+			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 5, Nome = "Teste de cadastros do nome E" });
+
+			return contexto;
+		}
+
+		[TestMethod]
+		public void SeConsultarPorCodigoDoContextoUmRegistroDoPoolDeveEncontrar()
+		{
+			var contexto = CriarContextoParaTestes();
+			var repositorio = contexto.Repositorio<ObjetoDeTestes>();
+
+			var query = repositorio.Buscar.CriarQuery()
+				.AdicionarCondicao(c => c.Codigo).Igual(5);
+
+			var registro = repositorio.Buscar.Um(query);
+
+			registro.Should().NotBeNull();
+			registro.Nome.Should().Be("Teste de cadastros do nome E");
+		}
+
+		[TestMethod]
+		public void SeConsultarPorVariosRegistrosNoContextoOsMesmosDevemSerEncontrados()
+		{
+			var contexto = CriarContextoParaTestes();
+			var repositorio = contexto.Repositorio<ObjetoDeTestes>();
+
+			var query = repositorio.Buscar.CriarQuery()
+				.AdicionarCondicao(c => c.Codigo).Seja(Operadores.MaiorOuIgual, 3)
+				.AdicionarCondicao(c => c.Codigo).Seja(Operadores.MenorOuIgual, 5);
+
+			var n = 3;
+
+			foreach (var registro in repositorio.Buscar.Varios(query))
+			{
+				registro.Should().NotBeNull();
+				registro.Codigo.Should().Be(n);
+				registro.Nome.Should().Be("Teste de cadastros do nome " + char.ConvertFromUtf32(64 + n));
+				n++;
+			}
+
+			n.Should().Be(6);
+		}
+
+		[TestMethod]
+		public void SeAdicionarUmaListaDeRegistrosNoContextoORepositorioNaoDeveSerModificadoEOsItensDevemSerEncontrados()
 		{
 			var contexto = new ContextoFake();
 
@@ -85,6 +152,10 @@ namespace RepositorioGenerico.Fake.Test.Contextos
 				ValidarItemDaListaPadrao(indice, item);
 				indice++;
 			}
+
+			indice
+				.Should()
+				.Be(2);
 		}
 
 		private static IList<ObjetoDeTestes> CriarListaPadrao()
--- Source/Tests/RepositorioGenerico.Fake.Test/FabricaFakeUnitTest.cs
+++ Source/Tests/RepositorioGenerico.Fake.Test/FabricaFakeUnitTest.cs
@@ -1,28 +1,12 @@
 ﻿using System;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using RepositorioGenerico.Pattern.Buscadores;
-using RepositorioGenerico.Pattern.Contextos;
-using RepositorioGenerico.Test.Objetos;
 
 namespace RepositorioGenerico.Fake.Test
 {
 	[TestClass]
 	public class FabricaFakeUnitTest
 	{
-
-		private IContexto CriarContextoParaTestes()
-		{
-			var contexto = FabricaFake.CriarContexto();
-
-			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 1, Nome = "Teste de cadastros do nome A" });
-			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 2, Nome = "Teste de cadastros do nome B" });
-			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 3, Nome = "Teste de cadastros do nome C" });
-			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 4, Nome = "Teste de cadastros do nome D" });
-			contexto.AdicionarRegistro(new ObjetoDeTestes() { Codigo = 5, Nome = "Teste de cadastros do nome E" });
-
-			return contexto;
-		}
 
 		[TestMethod]
 		public void SeCriarUmContextoFakeNaoDeveGerarErro()
@@ -39,88 +23,23 @@ namespace RepositorioGenerico.Fake.Test
 		}
 
 		[TestMethod]
-		public void SeCarregarContextoDeveConterRegistros()
+		public void SeCriarDoisContextoOsMesmosDevemSerDiferentes()
 		{
-			var contexto = CriarContextoParaTestes();
-			var repositorio = contexto.Repositorio<ObjetoDeTestes>();
-
-			repositorio.Quantidade
-				.Should().Be(0);
-
-			repositorio.Buscar.Todos()
+			var contextoA = FabricaFake.CriarContexto();
+			var contextoB = FabricaFake.CriarContexto();
+			contextoA
 				.Should()
-				.HaveCount(5);
+				.NotBe(contextoB);
 		}
 
 		[TestMethod]
-		public void SeConsultarPorCodigoUmRegistroDoPoolDeveEncontrar()
+		public void SeCriarDoisContextoLegadosOsMesmosDevemSerDiferentes()
 		{
-			var contexto = CriarContextoParaTestes();
-			var repositorio = contexto.Repositorio<ObjetoDeTestes>();
-
-			var query = repositorio.Buscar.CriarQuery()
-				.AdicionarCondicao(c => c.Codigo).Igual(5);
-
-			var registro = repositorio.Buscar.Um(query);
-
-			registro.Should().NotBeNull();
-			registro.Nome.Should().Be("Teste de cadastros do nome E");
-		}
-
-		[TestMethod]
-		public void SeConsultarPorVariosRegistrosDoPoolDeveEncontrar()
-		{
-			var contexto = CriarContextoParaTestes();
-			var repositorio = contexto.Repositorio<ObjetoDeTestes>();
-			
-			var query = repositorio.Buscar.CriarQuery()
-				.AdicionarCondicao(c => c.Codigo).Seja(Operadores.MaiorOuIgual, 3)
-				.AdicionarCondicao(c => c.Codigo).Seja(Operadores.MenorOuIgual, 5);
-
-			var n = 3;
-
-			foreach (var registro in repositorio.Buscar.Varios(query))
-			{
-				registro.Should().NotBeNull();
-				registro.Codigo.Should().Be(n);
-				registro.Nome.Should().Be("Teste de cadastros do nome " + char.ConvertFromUtf32(64 + n));
-				n++;
-			}
-
-			n.Should().Be(6);
-		}
-
-		[TestMethod]
-		public void SeConsultarPorNomeContendoDeveEncontrar()
-		{
-			var contexto = CriarContextoParaTestes();
-			var repositorio = contexto.Repositorio<ObjetoDeTestes>();
-
-			var query = repositorio.Buscar.CriarQuery()
-				.AdicionarCondicao(c => c.Nome).Contenha("nome C");
-
-			var registro = repositorio.Buscar.Um(query);
-
-			registro.Should().NotBeNull();
-			registro.Nome.Should().Be("Teste de cadastros do nome C");
-		}
-	
-		[TestMethod]
-		public void SeConsultarTodosDeveEncontrar()
-		{
-			var contexto = CriarContextoParaTestes();
-			var repositorio = contexto.Repositorio<ObjetoDeTestes>();
-
-			var n = 0;
-			foreach (var registro in repositorio.Buscar.Todos())
-			{
-				registro.Should().NotBeNull();
-				registro.Codigo.Should().Be(n + 1);
-				registro.Nome.Should().Be("Teste de cadastros do nome " + char.ConvertFromUtf32(65 + n));
-				n++;
-			}
-
-			n.Should().Be(5);
+			var contextoA = FabricaFake.CriarContextoLegado();
+			var contextoB = FabricaFake.CriarContextoLegado();
+			contextoA
+				.Should()
+				.NotBe(contextoB);
 		}
 
 	}
