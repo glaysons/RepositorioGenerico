68bf8ecfa9b2b9d6005ed9cc96d1a3003181da07
glaysons <glaysons@gmail.com>
2017-11-29 20:41:59 -0300

    Criação do projeto MVC

---
+++ Samples/Business.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Business.Mvc.MvcApplication" Language="C#" %>
---
+++ Samples/Business.Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Business.Mvc.Startup))]
+namespace Business.Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
