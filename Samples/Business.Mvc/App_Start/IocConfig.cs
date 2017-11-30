421c50aae79295a136a60f3d501e6d9e719e771e
glaysons <glaysons@gmail.com>
2017-11-29 21:48:04 -0300

    Configuração do módulo Ioc

---
+++ Samples/Business.Mvc/App_Start/IocConfig.cs
@@ -0,0 +1,59 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using Entities;
+using System.Configuration;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Business.Mvc
+{
+	public class IocConfig
+	{
+
+		public static void RegistrarIoc()
+		{
+			var builder = new ContainerBuilder();
+
+			RegistrarClassesDeNegocio(builder);
+
+			RegistrarControllersDoMvc(builder);
+
+			RegistrarConexaoPrincipal(builder);
+
+			RegistrarTodosOsValueObjects(builder);
+
+			InstalarIocNoMvc(builder);
+		}
+
+		private static void RegistrarClassesDeNegocio(ContainerBuilder builder)
+		{
+			builder.RegisterAssemblyTypes(typeof(ManutencaoCidadeBusiness).Assembly)
+				.Where(t => t.Name.EndsWith("Business"))
+				.InstancePerRequest();
+		}
+
+		private static void RegistrarControllersDoMvc(ContainerBuilder builder)
+		{
+			builder.RegisterControllers(typeof(IocConfig).Assembly)
+				.InstancePerRequest();
+		}
+
+		private static void RegistrarConexaoPrincipal(ContainerBuilder builder)
+		{
+			var cs = ConfigurationManager.ConnectionStrings["ConexaoPadrao"].ConnectionString;
+			RepositorioGenerico.SqlClient.Autofac.Configurador.RegisterSqlClient(builder, cs);
+		}
+
+		private static void RegistrarTodosOsValueObjects(ContainerBuilder builder)
+		{
+			RepositorioGenerico.SqlClient.Autofac.Configurador.RegisterModels(builder, typeof(Cidade).Assembly);
+		}
+
+		private static void InstalarIocNoMvc(ContainerBuilder builder)
+		{
+			builder.RegisterModule<AutofacWebTypesModule>();
+			var container = builder.Build();
+			DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+		}
+	}
+}
--- Samples/Business.Mvc/Global.asax.cs
+++ Samples/Business.Mvc/Global.asax.cs
@@ -5,13 +5,14 @@ using System.Web.Routing;
 namespace Business.Mvc
 {
 	public class MvcApplication : System.Web.HttpApplication
-    {
-        protected void Application_Start()
-        {
-            AreaRegistration.RegisterAllAreas();
-            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-    }
+	{
+		protected void Application_Start()
+		{
+			AreaRegistration.RegisterAllAreas();
+			FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+			RouteConfig.RegisterRoutes(RouteTable.Routes);
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			IocConfig.RegistrarIoc();
+		}
+	}
 }
