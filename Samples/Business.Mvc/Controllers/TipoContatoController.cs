4188376657eb47d756e2715945112f99247a37ba
glaysons <glaysons@gmail.com>
2017-12-01 13:51:18 -0300

    Criação das telas do gerenciamento de tipos dos contatos

---
+++ Samples/Business.Mvc/Controllers/TipoContatoController.cs
@@ -0,0 +1,110 @@
+﻿using Business.TiposDosContatos;
+using Entities;
+using RepositorioGenerico.Pattern.Contextos;
+using System;
+using System.Web.Mvc;
+
+namespace Business.Mvc.Controllers
+{
+	public class TipoContatoController : Controller
+	{
+		private IContexto _contexto;
+		private ConsultaTipoContatoBusiness _consulta;
+		private ManutencaoTipoContatoBusiness _manutencao;
+
+		public TipoContatoController(IContexto contexto, ConsultaTipoContatoBusiness consulta, ManutencaoTipoContatoBusiness manutencao)
+		{
+			_contexto = contexto;
+			_consulta = consulta;
+			_manutencao = manutencao;
+		}
+
+		public ActionResult Index()
+		{
+			return View(_consulta.ConsultarTodosOsTiposDosContatos());
+		}
+
+		public ActionResult Create()
+		{
+			return ExibirPaginaParaCriarOuEditar();
+		}
+
+		private ActionResult ExibirPaginaParaCriarOuEditar(TipoContato TipoContato = null)
+		{
+			ViewBag.Novo = (TipoContato == null);
+			return View("CreateEdit", TipoContato);
+		}
+
+		[HttpPost]
+		public ActionResult Create(TipoContato TipoContato)
+		{
+			ViewBag.MensagemErro = "";
+			try
+			{
+				_manutencao.Cadastrar(TipoContato);
+				_contexto.Salvar();
+				return RedirecionarParaPaginaInicialDaTipoContato();
+			}
+			catch (Exception ex)
+			{
+				ViewBag.MensagemErro = ex.Message;
+				return ExibirPaginaParaCriarOuEditar(TipoContato);
+			}
+		}
+
+		private ActionResult RedirecionarParaPaginaInicialDaTipoContato()
+		{
+			return Redirect("~/TipoContato");
+		}
+
+		public ActionResult Edit(int id)
+		{
+			return ExibirPaginaParaCriarOuEditar(_consulta.ConsultarTipoContato(id));
+		}
+
+		[HttpPost]
+		public ActionResult Edit(TipoContato TipoContato)
+		{
+			ViewBag.MensagemErro = "";
+			try
+			{
+				_manutencao.Atualizar(TipoContato);
+				_contexto.Salvar();
+				return RedirecionarParaPaginaInicialDaTipoContato();
+			}
+			catch (Exception ex)
+			{
+				ViewBag.MensagemErro = ex.Message;
+				return ExibirPaginaParaCriarOuEditar(TipoContato);
+			}
+		}
+
+		public ActionResult Details(int id)
+		{
+			return View(_consulta.ConsultarTipoContato(id));
+		}
+
+		public ActionResult Delete(int id)
+		{
+			return View(_consulta.ConsultarTipoContato(id));
+		}
+
+		[HttpPost]
+		public ActionResult Delete(TipoContato TipoContato)
+		{
+			ViewBag.MensagemErro = "";
+			try
+			{
+				_manutencao.Excluir(TipoContato);
+				_contexto.Salvar();
+				return RedirecionarParaPaginaInicialDaTipoContato();
+			}
+			catch (Exception ex)
+			{
+				ViewBag.MensagemErro = ex.Message;
+				return View(TipoContato);
+			}
+		}
+
+	}
+}
