8f50c22cbe5e264928a0951f5d09631a2cf48a37
Jonatan Waldow <jonwldw@gmail.com>
2020-05-27 13:51:32 -0400

    Adicionado summary no AutoIncrementoAttribute para melhor entendimento.

--- Source/RepositorioGenerico.Entities/Anotacoes/AutoIncrementoAttribute.cs
+++ Source/RepositorioGenerico.Entities/Anotacoes/AutoIncrementoAttribute.cs
@@ -9,6 +9,12 @@ namespace RepositorioGenerico.Entities.Anotacoes
 
 		public Incremento Incremento { get; set; }
 
+		/// <summary>
+		/// incremento - Nenhum: Você deverá fornecer o ID a ser gravado;
+		/// incremento - Identity: O SGBD calculará automaticamente o próximo ID;
+		/// incremento - Calculado: O Repositório Genérico calculará automaticamente o próximo ID;
+		/// </summary>
+		/// <param name="incremento"></param>
 		public AutoIncrementoAttribute(Incremento incremento)
 		{
 			Incremento = incremento;
